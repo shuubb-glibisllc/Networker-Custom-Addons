cce903804dbd29c1cc94fad2cdd778aaa4a60c34
GSTAdmin-IT <davitshubitidze@geosmartec.ge>
2025-09-04 12:36:56 +0400

    bettered leads import functionality

--- networker_contact/__manifest__.py
+++ networker_contact/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "18.0.1.0.0",
     "depends": ["base", "contacts", "crm"],
     "data": [
-        # "security/ir.model.access.csv",
+        "security/ir.model.access.csv",
         "views/res_partner_view.xml",
         "views/napr_fetch_wizard_views.xml",
         "views/partner_to_crm_wizard_views.xml",
